4fa0456447adbb2d95243859b1ea5ffaa1af3428
ak47 <kokitkarak@gmail.com>
2022-01-02 11:44:04 +0530

    Program to create and search binary tree

---
+++ BinarySearchTreeProblems/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace BinarySearchTreeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree!");
+
+            //UC1
+            BinarySearchTree<int> tree = new BinarySearchTree<int>(56);
+            tree.Insert(30);
+            tree.Insert(70);
+            tree.Display();
+            Console.WriteLine("---------------------------------------");
+
+            
+            //UC2
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Insert(22);
+            binarySearch.Insert(40);
+            binarySearch.Insert(60);
+            binarySearch.Insert(95);
+            binarySearch.Insert(11);
+            binarySearch.Insert(65);
+            binarySearch.Insert(3);
+            binarySearch.Insert(16);
+            binarySearch.Insert(63);
+            binarySearch.Insert(67);
+            binarySearch.Display();
+            binarySearch.GetSize();
+            Console.WriteLine("--------------------------------------");
+            
+
+            
+            //UC3
+            BinarySearchTree<int> binarySearch2 = new BinarySearchTree<int>(56);
+            binarySearch2.Insert(30);
+            binarySearch2.Insert(70);
+            binarySearch2.Insert(22);
+            binarySearch2.Insert(40);
+            binarySearch2.Insert(60);
+            binarySearch2.Insert(95);
+            binarySearch2.Insert(11);
+            binarySearch2.Insert(65);
+            binarySearch2.Insert(3);
+            binarySearch2.Insert(16);
+            binarySearch2.Insert(63);
+            binarySearch2.Insert(67);
+            binarySearch2.Display();
+            bool result = binarySearch2.Search(63, binarySearch2);
+            Console.WriteLine(result);
+            
+        }
+    }
+}
